05327d670e25903ed53440fc248400b1ce678322
Dax2t <97697794+Dax2t@users.noreply.github.com>
2024-01-19 11:34:43 +0100

    Update MainMenu.cpp

--- Solution/source/Submenus/MainMenu.cpp
+++ Solution/source/Submenus/MainMenu.cpp
@@ -8,12 +8,9 @@
 * (at your option) any later version.
 */
 #include "MainMenu.h"
-
 #include "..\macros.h"
-
 #include "..\Menu\Menu.h"
 #include "..\Menu\Routine.h"
-
 #include "..\Natives\natives2.h"
 
 namespace sub
@@ -21,24 +18,7 @@ namespace sub
 	void MainMenu()
 	{
 		AddTitle("MENYOO");
-
-		AddOption("Players", null, nullFunc, SUB::PLAYERSSUB);
-		AddOption("Player Options", null, nullFunc, SUB::PLAYEROPS);
-		AddOption("Vehicle Options", null, nullFunc, SUB::VEHICLEOPS);
-		AddOption("Teleport Options", null, nullFunc, SUB::TELEPORTOPS);
-		AddOption("Weapon Options", null, nullFunc, SUB::WEAPONOPS);
-		AddOption("Weather Options", null, nullFunc, SUB::WEATHEROPS);
-		AddOption("Time Options", null, nullFunc, SUB::TIMEOPS);
-		AddOption("Object Spooner", null, nullFunc, SUB::SPOONER_MAIN);
-		//AddOption("Lighting Options", null, nullFunc, SUB::OBJECTSPAWNER_LIGHTINGOPS);
 		AddOption("Misc Options", null, nullFunc, SUB::MISCOPS);
-		AddOption("Settings", null, nullFunc, SUB::SETTINGS);
-
-		Static_240 = PLAYER_ID();
-		Static_241 = PLAYER_PED_ID();
-		Static_239 = GET_PLAYER_NAME(Static_240);
-		Static_242 = GET_PLAYER_GROUP(Static_240);
-
 	}
 
 }
